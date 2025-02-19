265678b2cf4bdb06852c75edc8f9de1b91a99fff
Pa-ppy <rayjacks07@gmail.com>
2025-02-19 05:28:50 +0000

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,21 +4,26 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
+
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
 def close_storage(exception):
     """Close storage session"""
     storage.close()
 
-# Handle 404 errors
+
 @app.errorhandler(404)
 def not_found(error):
     """Return a JSON response for 404 errors"""
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
