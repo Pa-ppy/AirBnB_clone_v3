5525a0c635f0d35ef55693fc172152bc7d6f92c3
Pa-ppy <rayjacks07@gmail.com>
2025-02-19 04:15:50 +0000

    api

---
+++ api/v1/views/index.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Index route for API"""
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """Returns API status"""
+    return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Returns number of each object type"""
+    classes = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+    counts = {key: storage.count(value) for key, value in classes.items()}
+    return jsonify(counts)
